90f6785b54d54c8ac32d1424bda405f16323b8d3
bcdaniels <bdaniels@discovery.wisc.edu>
2014-10-31 22:10:07 -0400

    minimal functioning version
    Uses aubiopitch to detect pitch and gradually change output to match
    what is heard.  There is a pause in the output while listening for
    others.

--- cicada.py
+++ cicada.py
@@ -6,14 +6,92 @@
 
 import pyaudio
 import time
+import wave
+from subprocess import call
+import os
+import scipy
 
 # Hopefully these will be compatible with most microphone setups
 WIDTH = 2
 CHANNELS = 1
 RATE = 44100
+CHUNK = 1024
 
-if __name__ == '__main__':
+def saveWave(data,filename='output.wav'):
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(WIDTH)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+def detectPitch(data):
+    """
+    Returns an estimate of the dominant pitch in Hz
+    given PyAudio data.
     
+    Requires aubio command-line tool 'aubiopitch'.
+    http://aubio.org/
+    """
+    # 10.30.2014 At the moment, this (inefficiently!)
+    # saves the data to a wave file and then runs
+    # 'aubiopitch' on it.
+    # This is ultra-clunky.  I am coding on an airplane :)
+    
+    tmpPrefix = 'detectPitch_tmp'+str(os.getpid())
+    
+    tmpWaveName = tmpPrefix+'.wav'
+    saveWave(data,tmpWaveName)
+
+    tmpPitchName = tmpPrefix+'.txt'
+    tmpPitchFile = open(tmpPitchName,'w')
+    try:
+        call(["aubiopitch","-i",tmpWaveName],
+             stdout=tmpPitchFile)
+    except OSError, e:
+        raise Exception, "Problem calling aubiopitch: "+str(e)
+    tmpPitchFile.close()
+    
+    tmpPitchFile = open(tmpPitchName,'r')
+    # takes penultimate line of output as current pitch
+    lines = tmpPitchFile.readlines()
+    pitch = float(lines[-2].split(' ')[-1])
+    
+    # debug
+    #print ""
+    #print lines
+    
+    # clean up
+    os.remove(tmpPitchName)
+    os.remove(tmpWaveName)
+
+    return pitch
+
+def playTone(pitch,time=1):
+    """
+    Plays sinusoidal tone of given pitch (in Hz)
+    for given time (in seconds).
+    """
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                    channels=CHANNELS,
+                    rate=RATE,
+                    output=True)
+    floatData = scipy.sin( scipy.pi*float(pitch)/RATE* \
+        scipy.arange(int(time*RATE)))
+    chrData = [ chr(int(scipy.floor(d*127.+128.))) \
+        for d in floatData ]
+    stream.write(b''.join(chrData))
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+def wire():
+    """
+    Simple example of input audio -> output audio.
+    """
     # 10.28.2014 Example taken from 'Wire (Callback)'
     # at http://people.csail.mit.edu/hubert/pyaudio/
     #
@@ -30,6 +108,9 @@ if __name__ == '__main__':
         # to calculate the out_data.  For now, do something simple.
         out_data = in_data
         
+        #pitch = detectPitch(in_data)
+        #print pitch
+        
         return (out_data, pyaudio.paContinue)
 
     stream = p.open(format=p.get_format_from_width(WIDTH),
@@ -42,9 +123,87 @@ if __name__ == '__main__':
     stream.start_stream()
 
     while stream.is_active():
-        time.sleep(0.1)
+        time.sleep(1)
 
     stream.stop_stream()
     stream.close()
 
     p.terminate()
+
+def audioInput(samples=2048):
+    """
+    Return audio data from input channel (microphone).
+    By default, returns two "chunks" of 1024 samples
+    (This seems to be just enough to get a stable estimate
+    of the pitch.)
+    """
+    # 10.28.2014 Example taken from 'Wire (Callback)'
+    # at http://people.csail.mit.edu/hubert/pyaudio/
+    #
+    # At the moment it just outputs the audio it hears.
+    # Careful for feedback :)
+    
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    frames = []
+
+    for i in range(0,int(samples / CHUNK)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return b''.join(frames)
+
+def simpleCicada(learningRate=0.5,toneDur=(0.5,1.5),
+    initialPitch=440.,maxPitch=2000.,minPitch=0.):
+    """
+    If a pitch is heard, change myPitch according to
+        myPitch *= (heardPitch/myPitch)**learningRate.
+    
+    Tone is played for a random time in the range given
+    by toneDur (in seconds).
+    
+    Returns list of heard pitches and list of played pitches
+    (in Hz).
+    """
+    myPitch = initialPitch
+    heardPitchList,myPitchList = [],[]
+    
+    try:
+        while True:
+            # listen
+            heardPitch = detectPitch(audioInput())
+            
+            # change
+            if (heardPitch > minPitch) and (heardPitch < maxPitch):
+                myPitch *= (heardPitch/myPitch)**learningRate
+            print "heardPitch = %.1f, myPitch = %.1f"%(heardPitch,myPitch)
+        
+            # sing
+            dur = toneDur[0] \
+                + (toneDur[1]-toneDur[0])*scipy.random.rand()
+            playTone(myPitch,dur)
+            
+            # log
+            heardPitchList.append(heardPitch)
+            myPitchList.append(myPitch)
+
+    except KeyboardInterrupt:
+        return heardPitchList,myPitchList
+
+
+if __name__ == '__main__':
+
+    heardPitchList,myPitchList = simpleCicada()
+
+
+
