12b6f0cf49ef7f8f9def1e98e52fa140d4d1ad50
bcdaniels <bdaniels@discovery.wisc.edu>
2014-12-23 13:56:39 -0500

    adding initial javascript implementation
    combining “PitchDetect” from Chris Wilson and “Note Envelope” example
    from p5.js

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ To view a simple model of "cicada" activity: http://nbviewer.ipython.org/github/
 Dependencies
 ============
 
+For JavaScript implementation:
+
+- A server.  For testing, you can start a locally running server with Python using
+          
+     python -m SimpleHTTPServer
+
+     and then open a browser to http://localhost:8000 . 
+     Or, follow alternate instructions here: https://github.com/lmccart/p5.js/wiki/Local-server
+
+For Python implementation:
+
 - aubio command line tools: http://aubio.org/
 - pyaudio: http://people.csail.mit.edu/hubert/pyaudio/
 - PortAudio: http://www.portaudio.com [required only if building `pyaudio` from source]
---
+++ js/pitchoutput.js
@@ -0,0 +1,96 @@
+var osc, envelope, fft;
+
+/*
+BCD 12.23.2014
+Adapted from example at
+http://p5js.org/learn/examples/Sound__Note_Envelope.php
+*/
+
+function mapToInterval( pitch,minPitch,maxPitch ) {
+	/*
+    Given an arbitrary pitch, find the equivalent note
+    within the given pitch range (which by default spans
+    one octave).
+    */
+    var logIntervalRange = Math.log(maxPitch) - Math.log(minPitch);
+    var d = (Math.log(pitch)-Math.log(minPitch)) % logIntervalRange;
+    var mappedPitch = Math.exp(Math.log(minPitch) + d);
+
+    return mappedPitch;
+}
+
+var s = function( sketch ) {
+
+    var scaleArray = [60, 62, 64, 65, 67, 69, 71, 72];
+    var note = 0;
+    
+    var minPitchOut = 880.;
+    var maxPitchOut = 1760.;
+    
+    var maxPitchIn = 2000.;
+    
+    var learningRate = 0.5;
+
+    sketch.setup = function() {
+      sketch.createCanvas(710, 200);
+      osc = new p5.SinOsc();
+
+      // Instantiate the envelope with time / value pairs
+      envelope = new p5.Env(0.01, 0.5, 1, 0.5);
+
+      //osc.start();
+
+      fft = new p5.FFT();
+      sketch.noStroke();
+    };
+
+    sketch.draw = function() {
+      sketch.background(20);
+      
+      // BCD 12.23.2014 wait for sound input to start
+      var active = false;
+      detectorElem = document.getElementById( "detector" );
+      if (detectorElem) {
+        active = (detectorElem.className == "confident");
+      }
+      
+      if (active) {
+          // (*) Play myPitch
+          if (sketch.frameCount % 60 == 0) {
+            //var midiValue = scaleArray[note];
+            //var freqValue = sketch.midiToFreq(midiValue);
+            // BCD 12.23.2014
+            var myPitch = Math.round( noteElem.innerHTML );
+            var freqValue = myPitch;
+            osc.freq(freqValue);
+
+            envelope.play(osc);
+            note = (note + 1) % scaleArray.length;
+          }
+          
+          // (*) update myPitch between playing output notes
+          if (sketch.frameCount % 60 == 59) {
+            var heardPitch = pitch;
+            if (heardPitch < maxPitchIn) {
+                var mappedPitch = mapToInterval(heardPitch,minPitchOut,maxPitchOut);
+                //var mappedPitch = heardPitch;
+                var myPitch = Math.round( noteElem.innerHTML );
+                myPitch *= Math.pow(mappedPitch/myPitch,learningRate);
+                // Use PitchDetect's 'noteElem' text to display current output
+                noteElem.innerHTML = Math.round( myPitch );
+            }
+          }
+      }
+
+      // plot FFT.analyze() frequency analysis on the canvas 
+      var spectrum = fft.analyze();
+      for (var i = 0; i < spectrum.length/20; i++) {
+        sketch.fill(spectrum[i], spectrum[i]/10, 0);
+        var x = sketch.map(i, 0, spectrum.length/20, 0, sketch.width);
+        var h = sketch.map(spectrum[i], 0, 255, 0, sketch.height);
+        sketch.rect(x, sketch.height, spectrum.length/20, -h);
+      }
+    };
+};
+
+var myp5 = new p5(s);
