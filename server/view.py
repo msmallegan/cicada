2c176f79ddf92084d9c3fb48a246467ac2e7afd2
chebee7i <chebee7i@gmail.com>
2015-01-13 00:28:08 -0600

    Show only 30 most recent rows.

--- server/view.py
+++ server/view.py
@@ -5,15 +5,20 @@ Naive view of data.
 """
 from __future__ import print_function
 
+import datetime
 import sqlite3
 
 db = sqlite3.connect('frequencies.sqlite3')
 c = db.cursor()
-c.execute('SELECT * FROM frequencies;')
+c.execute('SELECT * FROM frequencies ORDER BY id DESC LIMIT 30;')
 
 print("Content-type: text/plain\n\n")
 print("id, session, location, date sent, date received, frequency")
 for row in c:
     print(row)
+
+# Note: Convert to datetime via:
+#    datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
+
 db.close()
 
